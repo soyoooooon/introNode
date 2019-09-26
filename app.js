7f93410a5e398817e8d931021c5182121f8ea3a9
이소윤 <soyoon@isoyuns-MBP.fc.ca>
2019-09-26 13:41:01 -0400

    connect to heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '127.0.0.1'; //this is a local host
-const port = 3000; // this is aa node connection
+const port = process.env.PORT || 3000; // this is aa node connection
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
