0c52e0a36a28d2b5692fe6d20b8f8e78b0de19d5
이소윤 <soyoon@isoyuns-MacBook-Pro.local>
2019-10-03 15:15:29 -0400

    initial build with Node.js

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-const http = require('http');
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+const sql = require('./utills/sql')
+const port = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1'; //this is a local host
-const port = process.env.PORT || 3000; // this is aa node connection
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.use(express.static('public'));
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname +"/views"))
+
+app.get('/',(req, res)=> { //slash is default view
+  res.render('home', { homemessage: "hi now" });
+})
+
+app.get('/users', (req, res) => {
+//get user data...... when we click this route
+
+
+//try a database connection
+//if the connection fails, log errors to the console
+
+sql.getConnection((err, connection) => {
+  if(err) {
+    return colsole.log(err.message);
+  }
+
+  let query = "SELECT * FROM tbl_card";
+  
+  sql.query(query, (err, rows) => {
+    connection.release();
+
+    if (err) { return console.log(err,message)}
+    console.log(rows);
+
+   res.render('user', rows[0]);
+  })
+})
+
+})
+
+
+
+
+app.listen(port,() => {
+  console.log(`app is running on port ${port}`);
 });
